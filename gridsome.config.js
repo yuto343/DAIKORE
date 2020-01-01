dcc777bb7b3a3b3d47656ac1a5600a8ab114e846
YutoNakano <namawasabi1229@gmail.com>
2020-01-01 17:12:30 +0900

    some changes

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,9 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: "Gridsome Blog Starter",
-  siteDescription:
-    "A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.",
+  siteName: "ダイジナノハコレカラ",
+  siteDescription: "Yuto Nakanoのブログ。",
 
   templates: {
     Post: "/:slug",
