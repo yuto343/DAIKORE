b6ca9288b66f9b99824c2f9d0b229304ae8440b5
YutoNakano <namawasabi1229@gmail.com>
2020-01-02 11:26:21 +0900

    setting global ogp image

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 // Import main css
-import '~/assets/style/index.scss'
+import "~/assets/style/index.scss";
 
 // Import default layout so we don't need to import it to every page
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-export default function (Vue, { router, head, isClient }) {
-  
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+
+  // Add a meta tag
+  head.meta.push({
+    name: "twitter:card",
+    content: "summary_large_image"
+  });
+  head.meta.push({
+    name: "og:url",
+    content: "https://yutonakano.com/ogp.png"
+  });
 }
