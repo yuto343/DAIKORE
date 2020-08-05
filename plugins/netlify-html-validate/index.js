f1e295acac0bce9c7aeb8ff9145f387fae7aa58c
yuto <namawasabi1229@gmail.com>
2020-08-05 23:39:51 +0900

    test

--- plugins/netlify-html-validate/index.js
+++ plugins/netlify-html-validate/index.js
@@ -1,19 +1,23 @@
 module.exports = {
   onPostBuild: async ({ utils, inputs, constants }) => {
     // console.log("hello from my html-validate!");
+    let directories = inputs.dir.split(",").map((dir) => {
+      return `${constants.PUBLISH_DIR}/${dir}`;
+    });
     try {
       await utils.run("html-validate", [
         ...(inputs.config ? ["--config", inputs.config] : []),
         "--ext",
         inputs.ext,
-        inputs.dir
-          ? inputs.dir
-              .split(",")
-              .map((dir) => {
-                return `${constants.PUBLISH_DIR}/${dir}`;
-              })
-              .join(" ")
-          : constants.PUBLISH_DIR,
+        ...(inputs.dir ? directories : []),
+        // inputs.dir
+        //   ? inputs.dir
+        //       .split(",")
+        //       .map((dir) => {
+        //         return `${constants.PUBLISH_DIR}/${dir}`;
+        //       })
+        //       .join(" ")
+        //   : constants.PUBLISH_DIR,
       ]);
     } catch (error) {
       if (error.exitCode === 1) {
