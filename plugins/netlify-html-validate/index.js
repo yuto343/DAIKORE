200c926f8d10f0d0ce48225e9af642323c6a2a43
yuto <namawasabi1229@gmail.com>
2020-08-05 23:21:43 +0900

    pluginを追加

---
+++ plugins/netlify-html-validate/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  onPostBuild: async ({ utils, inputs, constants }) => {
+    // console.log("hello from my html-validate!");
+    try {
+      await utils.run("html-validate", [
+        ...(inputs.config ? ["--config", inputs.config] : []),
+        "--ext",
+        inputs.ext,
+        ...(inputs.dir
+          ? inputs.dir
+              .split(",")
+              .map((dir) => {
+                return `${constants.PUBLISH_DIR}/${dir}`;
+              })
+              .join(" ")
+          : constants.PUBLISH_DIR),
+      ]);
+    } catch (error) {
+      if (error.exitCode === 1) {
+        return utils.build.failBuild("Invalid HTML", { error });
+      }
+      return utils.build.failBuild("Unknown error", { error });
+    }
+  },
+};
